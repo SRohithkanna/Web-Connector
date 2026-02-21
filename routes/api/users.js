8f381ee83fcf4307b4088cb8cbf49a029c5e9579
SRohithkanna <srohithkanna@gmail.com>
2026-02-22 02:00:03 +0530

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,58 @@
 const express= require('express');
 const router= express.Router();
+const gravatar= require('gravatar')
+const bcrypt=require('bcryptjs')
+const {check,validationResult}= require('express-validator')
 
-// @route: GET api/users
+const User=require('../../models/User');
+// @route: POST api/users
 // @desc: test route
 // @access: public
-router.get('/',(req,res) => res.send('User route'));
+router.post('/',[
+    check('name','name is required').not().isEmpty(),
+    check('email','Please include valid email').isEmail(),
+    check('password','Please enter with 6 or more chr').isLength({min:6})    
+],
+async (req,res) => {
+    const errors= validationResult(req);
+    //if error(does not match the check function), 400 ->bad request
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {name,email,password}= req.body;
+
+    try{
+        //see if user exists
+        let user=await User.findOne({email});
+        if(user){
+           return res.status(400).json({errors: [{msg: 'User already exist'}]});
+        }
+        //get users gravatar
+        const avatar=gravatar.url(email,{
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        user=new User({
+            name,
+            email,
+            avatar,
+            password
+
+        });
+        //encrypt password(password list in DB is encrypted)
+        const salt= await bcrypt.genSalt(10);
+
+        user.password= await bcrypt.hash(password,salt);
+        await user.save();
+        
+        res.send('User registered');
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 //connect database
 connectDB();
 
+//intialize middleware
+app.use(express.json({extended:false}));
 app.get('/', (req, res) => res.send('API running'));
 
 //define routes
